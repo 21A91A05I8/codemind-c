8c0a56718af7d4229becb5eec70e0597c0c46ff1
21A91A05I8 <97424594+21A91A05I8@users.noreply.github.com>
2022-04-19 22:46:16 +0530

    #include<stdio.h> int main() {     int num,sq1,sq2,rev1=0,rev2=0;     scanf("%d",&num);     sq1=num*num;     do     {         rev1=rev1*10;         rev1=rev1+num%10;         num=num/10;     }     while(num>0);     sq2=rev1*rev1;     do     {         rev2=rev2*10;         rev2=rev2+sq2%10;         sq2=sq2/10;     }     while(sq2>0);     if(rev2==sq1)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Adam_number.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int num,sq1,sq2,rev1=0,rev2=0;
+    scanf("%d",&num);
+    sq1=num*num;
+    do
+    {
+        rev1=rev1*10;
+        rev1=rev1+num%10;
+        num=num/10;
+    }
+    while(num>0);
+    sq2=rev1*rev1;
+    do
+    {
+        rev2=rev2*10;
+        rev2=rev2+sq2%10;
+        sq2=sq2/10;
+    }
+    while(sq2>0);
+    if(rev2==sq1)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
