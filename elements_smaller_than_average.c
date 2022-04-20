4ae932392fa88107d2a18360a030758348e51516
21A91A05I8 <97424594+21A91A05I8@users.noreply.github.com>
2022-04-20 18:26:09 +0530

    #include<stdio.h> int main() {     int i,n,c=0,arr[100],avg=0,sum=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);         sum=sum+arr[i];     }     avg=sum/n;     for(i=0;i<n;i++)     {         if(arr[i]<=avg)         {             c++;         }
        }
        printf("%d",c);
    }

---
+++ elements_smaller_than_average.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,c=0,arr[100],avg=0,sum=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        sum=sum+arr[i];
+    }
+    avg=sum/n;
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]<=avg)
+        {
+            c++;
+        }
+        
+    }
+    printf("%d",c);
+}
