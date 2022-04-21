2a91618b4f3449332391ed1c7f58924cb2c5caf5
21A91A05I8 <97424594+21A91A05I8@users.noreply.github.com>
2022-04-21 20:58:38 +0530

    #include<stdio.h> #include<math.h> int main() {     int p,t,r;     double c;     scanf("%d%d%d",&p,&r,&t);     c=p*pow((1+(r/100.0)),t);     printf("%.2lf",c);     return 0;
    }

---
+++ Compound_interest.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int p,t,r;
+    double c;
+    scanf("%d%d%d",&p,&r,&t);
+    c=p*pow((1+(r/100.0)),t);
+    printf("%.2lf",c);
+    return 0;
+    
+}
