feefcd1048a3d535638074d29a2dda5d5d6862a1
21A91A05I8 <97424594+21A91A05I8@users.noreply.github.com>
2022-05-04 11:29:45 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,j,c=1,k,dc=0,sum=0;     float y;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         c=1;         for(j=0;j<n;j++)         {             if(arr[i]==arr[j]&& i!=j && arr[i]>=0)             {                 c++;                 arr[j]=-1;
                }
               
            }
            if(c==arr[i])
            
           {
               
              dc++;
              sum=sum+arr[i];
              
            }
        }
        
        if(dc==0)
        {
            printf("-1");
        }
        else
        {
            y=float(sum)/dc;
            printf("%.2f",y);
        }
        
    }

---
+++ Average_of_super_elements.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,j,c=1,k,dc=0,sum=0;
+    float y;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=1;
+        for(j=0;j<n;j++)
+        {
+            if(arr[i]==arr[j]&& i!=j && arr[i]>=0)
+            {
+                c++;
+                arr[j]=-1;
+                
+                
+            }
+           
+        }
+        if(c==arr[i])
+        
+       {
+           
+          dc++;
+          sum=sum+arr[i];
+          
+        }
+    }
+    
+    if(dc==0)
+    {
+        printf("-1");
+    }
+    else
+    {
+        y=float(sum)/dc;
+        printf("%.2f",y);
+    }
+    
+}
