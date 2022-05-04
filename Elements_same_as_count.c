6160c8b0e2d0671ff829a19a46cc75ef820bd65b
21A91A05I8 <97424594+21A91A05I8@users.noreply.github.com>
2022-05-04 10:53:22 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,j,c=1,k,dc=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d",&k);     for(i=0;i<n;i++)     {         c=1;         for(j=0;j<n;j++)         {             if(arr[i]==arr[j]&& i!=j && arr[j]!=-1)             {                 c++;                 arr[j]=-1;
                }
               
            }
            if(c==arr[i])
            
           {
               
              dc++;
              printf("%d ",arr[i]);
            }
        }
        if(dc==0)
        {
            printf("-1");
        }
        
    }

---
+++ Elements_same_as_count.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,j,c=1,k,dc=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d",&k);
+    for(i=0;i<n;i++)
+    {
+        c=1;
+        for(j=0;j<n;j++)
+        {
+            if(arr[i]==arr[j]&& i!=j && arr[j]!=-1)
+            {
+                c++;
+                arr[j]=-1;
+                
+                
+            }
+           
+        }
+        if(c==arr[i])
+        
+       {
+           
+          dc++;
+          printf("%d ",arr[i]);
+        }
+    }
+    if(dc==0)
+    {
+        printf("-1");
+    }
+    
+}
