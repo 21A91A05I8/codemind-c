2d2d869865c5e2531d7ecadb77fde49198ce740a
21A91A05I8 <97424594+21A91A05I8@users.noreply.github.com>
2022-05-06 09:30:22 +0530

    #include<stdio.h> int main() {     int n,arr[100],a,b,min=999,i,c=0;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d%d",&a,&b);     for(i=1;i<=n;i++)     {         if(arr[i]>=a && arr[i]<=b)         {             if(arr[i]<min)             {                 c++;                 min=arr[i];             }
            }
        }
       if(c!=0)
       {
           printf("%d",min);
       }
      else
       {
           printf("-1");
       }
        
        
    }

---
+++ minimum_between_A_and_B.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],a,b,min=999,i,c=0;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d%d",&a,&b);
+    for(i=1;i<=n;i++)
+    {
+        if(arr[i]>=a && arr[i]<=b)
+        {
+            if(arr[i]<min)
+            {
+                c++;
+                min=arr[i];
+            }
+            
+        }
+    }
+   if(c!=0)
+   {
+       printf("%d",min);
+   }
+  else
+   {
+       printf("-1");
+   }
+    
+    
+}
+
