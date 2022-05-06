c8b7c7741cd8f0e224161a4a86225d28985b3b4c
21A91A05I8 <97424594+21A91A05I8@users.noreply.github.com>
2022-05-06 11:37:32 +0530

    #include<stdio.h> int main() {     int i,sum=0,n,arr[100],s=0;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         scanf("%d",&arr[i]);     }     if(n%2==0)     {     for(i=1;i<=n/2;i++)     {
                sum=sum+arr[i];
            
        }
         for(i=(n/2)+1;i<=n;i++)
        {
            
                s=s+arr[i];
            
        }
        printf("%d
    %d",sum,s);
        }
        if(n%2==1)
        {
           for(i=1;i<(n+1)/2;i++)
        {
            
                sum=sum+arr[i];
            
        }
         for(i=(n+1)/2;i<=n;i++)
        {
            
                s=s+arr[i];
            
        }
        printf("%d
    %d",sum,s); 
        }
        
        
    }

---
+++ sum_of_first_half_and_second_half_in_an_array.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+int main()
+{
+    int i,sum=0,n,arr[100],s=0;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    if(n%2==0)
+    {
+    for(i=1;i<=n/2;i++)
+    {
+        
+            sum=sum+arr[i];
+        
+    }
+     for(i=(n/2)+1;i<=n;i++)
+    {
+        
+            s=s+arr[i];
+        
+    }
+    printf("%d
+%d",sum,s);
+    }
+    if(n%2==1)
+    {
+       for(i=1;i<(n+1)/2;i++)
+    {
+        
+            sum=sum+arr[i];
+        
+    }
+     for(i=(n+1)/2;i<=n;i++)
+    {
+        
+            s=s+arr[i];
+        
+    }
+    printf("%d
+%d",sum,s); 
+    }
+    
+    
+}
