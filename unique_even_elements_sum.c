34803c4685373c0fb25b321a444151a9245a1905
21A91A05I8 <97424594+21A91A05I8@users.noreply.github.com>
2022-06-01 21:11:43 +0530

    #include<stdio.h> int main() {     int n,arr[1000],i,j,c=0,dc=0,s=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         c=0;         for(j=i+1;j<=n;j++)         {             if(arr[i]==arr[j] )             {                 c++;             }         }         if(c==0)         {             if(arr[i]%2==0)             {                 s=s+arr[i];             }         }     }     printf("%d",s);     return 0; }

---
+++ unique_even_elements_sum.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[1000],i,j,c=0,dc=0,s=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=i+1;j<=n;j++)
+        {
+            if(arr[i]==arr[j] )
+            {
+                c++;
+            }
+        }
+        if(c==0)
+        {
+            if(arr[i]%2==0)
+            {
+                s=s+arr[i];
+            }
+        }
+    }
+    printf("%d",s);
+    return 0;
+}
