7c88528d195329fa9df091e2d5b704a949f7cf63
21A91A05I8 <97424594+21A91A05I8@users.noreply.github.com>
2022-06-01 21:09:02 +0530

    #include<stdio.h> int main() {     int n,arr[1000],i,j,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         c=0;         for(j=0;j<=n;j++)         {             if(arr[i]==arr[j]&&i!=j )             {                 arr[j]=-1;             }         }
        }
        for(j=0;j<n;j++)
        {
            if(arr[j]!=-1)
            {
                printf("%d ",arr[j]);
            }
        }
    }

---
+++ unique_elements.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[1000],i,j,c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=0;j<=n;j++)
+        {
+            if(arr[i]==arr[j]&&i!=j )
+            {
+                arr[j]=-1;
+            }
+        }
+       
+    }
+    for(j=0;j<n;j++)
+    {
+        if(arr[j]!=-1)
+        {
+            printf("%d ",arr[j]);
+        }
+    }
+}
