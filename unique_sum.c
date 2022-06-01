7d0e2d77ac824bb4c4b97a8ce8207aac7d083398
21A91A05I8 <97424594+21A91A05I8@users.noreply.github.com>
2022-06-01 20:48:04 +0530

    #include<stdio.h> int main() {     int n,arr[1000],i,j,c=0,dc=0,sum=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         c=0;         for(j=i+1;j<=n;j++)         {             if(arr[i]==arr[j] )             {                 c++;             }         }         if(c==0)         {             sum=sum+arr[i];         }     }     printf("%d",sum);     return 0; }

---
+++ unique_sum.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[1000],i,j,c=0,dc=0,sum=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=i+1;j<=n;j++)
+        {
+            if(arr[i]==arr[j] )
+            {
+                c++;
+            }
+        }
+        if(c==0)
+        {
+            sum=sum+arr[i];
+        }
+    }
+    printf("%d",sum);
+    return 0;
+}
